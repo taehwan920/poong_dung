730d37321cc308895244c25ceddd13f98478aaeb
Yang Taehwan <taehwan920@gmail.com>
2020-01-09 23:09:23 +0900

    Using API because web crawler doesnt work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import axios from "axios";
 // import './App.css';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        waterDatas: []
+    };
+    getWaterDatas = async () => {
+        const waterDatas = await axios.get("http://hangang.dkserver.wo.tc/")
+
+    }
+    componentDidMount() {
+        this.getWaterDatas();
+    }
     render() {
+        const { isLoading } = this.state;
         return (
-            <h1>오늘 한강 수온은?</h1>
+            <h1>{isLoading ? "Loading..." : "오늘 한강 수온은?"}</h1>
         )
     }
 }
--- web_crawler/han_gang.py
+++ web_crawler/han_gang.py
@@ -1,15 +1,14 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-URL = "http://koreawqi.go.kr/index_web.jsp"
+# URL = "http://koreawqi.go.kr/index_web.jsp"
 
-def get_temperature():
-    req_url = requests.get(URL)
-    soup = BeautifulSoup(req_url.text, 'html.parser')
-    parent = soup.find("tr", {"class" : "site_S01001"})
-    temper = parent.findChild("td", {"class" : "avg1"})
-    tem_text = temper.get_text(strip=True).strip()
-    print(tem_text)
-    return {"temperature" : tem_text}
+# def get_temperature():
+#     req_url = requests.get(URL)
+#     soup = BeautifulSoup(req_url.text, 'html.parser')
+#     parent = soup.find("tr", {"class" : "site_S01001"})
+#     temper = parent.findChild("td", {"class" : "avg1"})
+#     tem_text = temper.get_text(strip=True).strip()
+#     return {"temperature" : tem_text}
 
-get_temperature()
+# get_temperature()
